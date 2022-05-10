5983d1c9a6b11e2e22337893c0a9fd262575d251
Zac-Mwangi <zackmwangi998@gmail.com>
2022-05-10 23:02:00 +0300

    upper and lower cases in js

---
+++ loweCase_UpperCase.js
@@ -0,0 +1,10 @@
+// Uppercase
+var name = "moringa";
+var uppercase = name.toUpperCase();
+console.log(uppercase)
+
+
+// lowerCase
+var name2 = "KELVIN";
+var lowerCase = name2.toLowerCase();
+console.log(lowerCase)
